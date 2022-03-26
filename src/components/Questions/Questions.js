bf8680c07ceb10cb4e315914c06935b81ae6b21d
subrata sarker <tccdcc8@gmail.com>
2022-03-26 22:17:15 +0600

    check full

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -10,7 +10,7 @@ const Questions = () => {
                 put props is specific it will take real data information.when need a parameter in a component then use props . If user want state will be change </p>
             <h2>Question no 2: How useState Work </h2>
             <p>Ans: useState is a react hook . useState return an array and a function. Array store data which data is change or not change. Function need to update array data. it helps to avoid multiline code. help to clear code area. when declear usestate it need a initial value. this is the example <br />
-                const [cart, setCart]=useState()
+                const [cart, setCart]=useState();
             </p>
         </div>
     );
