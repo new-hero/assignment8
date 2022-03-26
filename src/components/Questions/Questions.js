a82431f105703b01b6d22039963b3e2a31874131
subrata sarker <tccdcc8@gmail.com>
2022-03-26 22:04:44 +0600

    answer is done

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -5,9 +5,13 @@ const Questions = () => {
     return (
         <div className='question'>
             <h2>Question no 1: Difference Between props and state </h2>
-            <p> </p>
+            <p> Answer: Props use for sent data or information from the parent to child.
+                state is use for variation purpase.state change or not change both depende on user.
+                put props is specific it will take real data information.when need a parameter in a component then use props . If user want state will be change </p>
             <h2>Question no 2: How useState Work </h2>
-            <p></p>
+            <p>Ans: useState is a react hook . useState return an array and a function. Array store data which data is change or not change. Function need to update array data. it helps to avoid multiline code. help to clear code area. when declear usestate it need a initial value. this is the example <br />
+                const [cart, setCart]=useState()
+            </p>
         </div>
     );
 };
